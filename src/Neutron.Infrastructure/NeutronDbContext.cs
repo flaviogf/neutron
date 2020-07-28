65824bef7f2ff48e33b7392fffe27d05be6a486a
flaviogf <flavio.fernandes6@gmail.com>
2020-07-28 13:53:47 -0300

    feat: adding NeutronDbContext

---
+++ src/Neutron.Infrastructure/NeutronDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Neutron.Core;
+
+namespace Neutron.Infrastructure
+{
+    public class NeutronDbContext : DbContext
+    {
+        public NeutronDbContext(DbContextOptions<NeutronDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Event> Events { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder
+                .Entity<Event>()
+                .HasKey(it => it.Id);
+
+            modelBuilder
+                .Entity<Event>()
+                .Property(it => it.Name)
+                .IsRequired();
+
+            modelBuilder
+                .Entity<Event>()
+                .Property(it => it.Target)
+                .IsRequired();
+        }
+    }
+}
