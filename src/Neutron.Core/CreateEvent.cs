b38d5b1077b8f6ca242ef87c2b00e40f23fd6e4d
flaviogf <flavio.fernandes6@gmail.com>
2020-07-28 11:24:53 -0300

    feat: adding CreateEvent

---
+++ src/Neutron.Core/CreateEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CSharpFunctionalExtensions;
+using MediatR;
+
+namespace Neutron.Core
+{
+    public sealed class CreateEvent : IRequest<Result>
+    {
+        public CreateEvent(Guid id, string name, DateTime? target)
+        {
+            Id = id;
+            Name = name;
+            Target = target;
+        }
+
+        public Guid Id { get; }
+
+        public string Name { get; }
+
+        public DateTime? Target { get; }
+    }
+}
