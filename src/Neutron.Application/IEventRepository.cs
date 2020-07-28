3b079f2717dcd0c6fe993e9f3acaec4fca0bd1e9
flaviogf <flavio.fernandes6@gmail.com>
2020-07-28 11:25:38 -0300

    feat: adding EventRepository

---
+++ src/Neutron.Application/IEventRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+using Neutron.Core;
+
+namespace Neutron.Application
+{
+    public interface IEventRepository
+    {
+        Task<Result> Add(Event @event);
+    }
+}
