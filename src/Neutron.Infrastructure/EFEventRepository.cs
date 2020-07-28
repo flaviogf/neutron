0803a4e35b4c3ea35f2a5129de8da180996779c0
flaviogf <flavio.fernandes6@gmail.com>
2020-07-28 13:54:06 -0300

    feat: adding EFEventRepository

---
+++ src/Neutron.Infrastructure/EFEventRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+using Neutron.Application;
+using Neutron.Core;
+
+namespace Neutron.Infrastructure
+{
+    public class EFEventRepository : IEventRepository
+    {
+        private readonly NeutronDbContext _context;
+
+        public EFEventRepository(NeutronDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Result> Add(Event @event)
+        {
+            await _context.Events.AddAsync(@event);
+
+            return Result.Success();
+        }
+    }
+}
