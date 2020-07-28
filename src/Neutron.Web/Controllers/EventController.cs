48a46f822f9b979b52aa8ed349adfbb7aa853de4
flaviogf <flavio.fernandes6@gmail.com>
2020-07-28 13:59:59 -0300

    feat: adding EventController

---
+++ src/Neutron.Web/Controllers/EventController.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using CSharpFunctionalExtensions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Neutron.Core;
+using Neutron.Infrastructure;
+using Neutron.Web.ViewModels;
+
+namespace Neutron.Web.Controllers
+{
+    [Route("[controller]")]
+    public class EventController : Controller
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+
+        public EventController(IUnitOfWork uow, IMediator mediator, IMapper mapper)
+        {
+            _uow = uow;
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        [HttpGet("Create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([FromForm] CreateEventViewModel viewModel)
+        {
+            CreateEvent createEvent = _mapper.Map<CreateEvent>(viewModel);
+
+            Result result = await _mediator.Send(createEvent);
+
+            if (result.IsFailure)
+            {
+                TempData["Failure"] = result.Error;
+
+                return RedirectToAction("Create", "Event");
+            }
+
+            TempData["Success"] = "Event has been created";
+
+            _uow.Commit();
+
+            return RedirectToAction("Create", "Event");
+        }
+    }
+}
