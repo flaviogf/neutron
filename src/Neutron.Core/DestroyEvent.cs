4dc98183a1a27e616a7ca93b943c28026f1ea25c
flaviogf <flavio.fernandes6@gmail.com>
2020-07-29 14:49:20 -0300

    feat: adding DestroyEvent request

---
+++ src/Neutron.Core/DestroyEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CSharpFunctionalExtensions;
+using MediatR;
+
+namespace Neutron.Core
+{
+    public sealed class DestroyEvent : IRequest<Result>
+    {
+        public DestroyEvent(Guid id)
+        {
+            Id = id;
+        }
+
+        public Guid Id { get; }
+    }
+}
