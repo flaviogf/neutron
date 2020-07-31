d2e9cafeb831c7d3362d6596d0bc8d85a8a00f5e
flaviogf <flavio.fernandes6@gmail.com>
2020-07-31 08:55:55 -0300

    feat: adding identity db context

---
+++ src/Neutron.Infrastructure/IdentityDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Neutron.Infrastructure
+{
+    public class IdentityDbContext : Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityDbContext<ApplicationUser, ApplicationRole, string>
+    {
+        public IdentityDbContext(DbContextOptions<IdentityDbContext> options) : base(options)
+        {
+        }
+    }
+}
