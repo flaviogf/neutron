f06a21da8f9de576cd4d66dd505f5cdf7b566f53
flaviogf <flavio.fernandes6@gmail.com>
2020-07-31 10:59:44 -0300

    feat: adding SignOut Controller

---
+++ src/Neutron.Web/Controllers/SignOutController.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Neutron.Infrastructure;
+
+namespace Neutron.Web.Controllers
+{
+    [Route("[controller]")]
+    public class SignOutController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public SignOutController(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Destroy()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Create", "SignIn");
+        }
+    }
+}
