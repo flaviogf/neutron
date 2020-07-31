2afbb22c93ccb7d44e8fe7aa02ce46e57ad072fa
flaviogf <flavio.fernandes6@gmail.com>
2020-07-31 09:20:13 -0300

    feat: adding CreateSignInViewModel

---
+++ src/Neutron.Web/ViewModels/CreateSignInViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Neutron.Web.ViewModels
+{
+    public class CreateSignInViewModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
