da421c3ce15b36ca092de6db3a58f4d1a768f776
flaviogf <flavio.fernandes6@gmail.com>
2020-08-03 17:59:31 -0300

    feat: connecting client with server by signalr

---
+++ src/Neutron.Web/wwwroot/js/event/show.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    var connection = new signalR.HubConnectionBuilder()
+        .withUrl("/eventHub")
+        .withAutomaticReconnect()
+        .build();
+
+    connection.on("Tack", function (event) {
+        var timeLeft = event.timeLeft
+        var days = timeLeft.days.toString().padStart(2, '0')
+        var hours = timeLeft.hours.toString().padStart(2, '0')
+        var minutes = timeLeft.minutes.toString().padStart(2, '0')
+        var seconds = timeLeft.seconds.toString().padStart(2, '0')
+        var text = `Days: ${days} Hours: ${hours}:${minutes}:${seconds}`;
+        $('[data-time-left]').html(text)
+    });
+
+    connection.start().then(function () {
+        var eventId = $('[name=Id]').val();
+        connection.invoke("Tick", eventId);
+    });
+});
