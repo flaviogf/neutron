b406c93126d9dd11fc049602b5e2c302467dba16
flaviogf <flavio.fernandes6@gmail.com>
2020-08-03 17:54:58 -0300

    test: testing behavior of new properties at Event

---
+++ test/Neutron.Test/EventHasArrivedTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Neutron.Core;
+using Xunit;
+
+namespace Neutron.Test
+{
+    public class EventHasArrivedTest
+    {
+        [Fact]
+        public void Should_Be_True_When_Target_Has_Arrived()
+        {
+            DateTime yesterday = DateTime.Now.AddDays(-1);
+
+            Event @event = new Event(Guid.NewGuid(), "Yesterday", yesterday);
+
+            Assert.True(@event.HasArrived);
+        }
+
+        [Fact]
+        public void Should_Be_False_When_The_Target_Has_Not_Arrived()
+        {
+            DateTime tomorrow = DateTime.Now.AddDays(1);
+
+            Event @event = new Event(Guid.NewGuid(), "Tomorrow", tomorrow);
+
+            Assert.False(@event.HasArrived);
+        }
+    }
+}
---
+++ test/Neutron.Test/EventHasNotArrivedTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Neutron.Core;
+using Xunit;
+
+namespace Neutron.Test
+{
+    public class EventHasNotArrivedTest
+    {
+        [Fact]
+        public void Should_Be_True_When_Target_Has_Not_Arrived()
+        {
+            DateTime tomorrow = DateTime.Today.AddDays(1);
+
+            Event @event = new Event(Guid.NewGuid(), "Tomorrow", tomorrow);
+
+            Assert.True(@event.HasNotArrived);
+        }
+
+        [Fact]
+        public void Should_Be_False_When_Target_Has_Arrived()
+        {
+            DateTime yesterday = DateTime.Today.AddDays(-1);
+
+            Event @event = new Event(Guid.NewGuid(), "Yesterday", yesterday);
+
+            Assert.False(@event.HasNotArrived);
+        }
+    }
+}
