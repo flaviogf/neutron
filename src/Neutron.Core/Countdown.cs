3e28c37c83a5c9705dccb1768d5c78b2b81b5d19
flaviogf <flavio.fernandes6@gmail.com>
2020-08-03 17:55:54 -0300

    feat: adding Countdown

---
+++ src/Neutron.Core/Countdown.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Neutron.Core
+{
+    public class Countdown
+    {
+        public Countdown(Event @event)
+        {
+            Event = @event;
+        }
+
+        public Event Event { get; }
+
+        public event EventHandler Changed;
+
+        public async Task Start(CancellationToken token)
+        {
+            while (Event.HasNotArrived && !token.IsCancellationRequested)
+            {
+                OnChange();
+
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+        }
+
+        private void OnChange()
+        {
+            Changed?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
