5a2cec0f01399167ec87d80ff1d453f3316a7f92
flaviogf <flavio.fernandes6@gmail.com>
2020-08-04 07:35:23 -0300

    feat: adding new events to event client

--- src/Neutron.Web/Hubs/IEventClient.cs
+++ src/Neutron.Web/Hubs/IEventClient.cs
@@ -6,5 +6,9 @@ namespace Neutron.Web.Hubs
     public interface IEventClient
     {
         Task Tack(Event @event);
+
+        Task Start(Event @event);
+
+        Task Stop(Event @event);
     }
 }
